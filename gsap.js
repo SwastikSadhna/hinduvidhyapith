a7a253ca324416919706b0eeb1e209e9cb29137b
Neesargsoni <neesargs@gmail.com>
2023-07-28 19:57:49 +0530

    updation in animation by neesarg

--- gsap.js
+++ gsap.js
@@ -1,26 +1,41 @@
 var a=gsap.timeline();
 
-a.from(".navbar",{
+a.from(".navbar-brand",{
     y:-100,
     opacity:0,
-    duration:0.5,
-    delay:0.5
+    duration:0.8,
+    delay:0.3
 })
-
+a.from("#items",{
+    y:-120,
+    opacity:0,
+    duration:0.6,
+    stagger:0.2
+})
+a.from(".both",{
+    y:-110,
+    opcaity:0,
+    duration:0.3
+})
 a.from(".carousel-indicators,.carousel-inner,.carousel-item",{
     opacity:0,
-    duration:0.5,
+    duration:0.5
 })
 a.to("h2",{
     opcaity:0,
-    duration:0.5,
-    
+    duration:0.5
 })
-a.from("#card-body,#card2,#card3",{
+a.from(".card",{
     scale:0,
+    rotate:-360,
     opacity:0,
-    duration:0.5,
-    stagger:0.4
+    duration:0.7,
+    stagger:0.4,
+    // scrollTrigger:{
+    //     trigger:".card",
+    //     scroller:
+    //     markers:true,
+    // }
 })
 
 gsap.from("#custom-cards,#custom-card2,#custom-card3",{
